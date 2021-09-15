17933affe9660f8989e5bacb4a14de650699f6c7
LarsAlex20 <90389063+LarsAlex20@users.noreply.github.com>
2021-09-15 13:54:26 +0200

    re

--- Mastermind/ViewModels/ViewModelGameBoard.cs
+++ Mastermind/ViewModels/ViewModelGameBoard.cs
@@ -56,12 +56,7 @@ namespace Mastermind.ViewModels
             GameBoardData.Try = "Restliche Versuche: " + Data.InputVersuche;
 
             ColorList = FillColorList(farben);
-            //ComputerCode.generieren(ColorList);
-            ComputerCode.setPin(0, new Farbpin(Colors.Blue));
-            ComputerCode.setPin(1, new Farbpin(Colors.Violet));
-            ComputerCode.setPin(2, new Farbpin(Colors.LightSkyBlue));
-            ComputerCode.setPin(3, new Farbpin(Colors.Yellow));
-            ComputerCode.setPin(4, new Farbpin(Colors.Blue));
+            ComputerCode.generieren(ColorList);
 
             for (int i = 0; i < Data.InputCodeLaenge; i++)
             {
